769c4624d57d16250e865916502a83cb7c309aa4
Eduardo Neira <edun327@gmail.com>
2019-03-07 19:51:59 -0300

    First changes to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -42,46 +42,18 @@
       >
         <v-icon>web</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :fixed="fixed"
       app
-    >
-      <span>&copy; 2019</span>
+     >
+      <v-flex class="text-xs-right">Big Fiubrother - Eduardo Neira - Gabriel Gayoso &copy; 2019 &nbsp;</v-flex>
     </v-footer>
   </v-app>
 </template>
@@ -92,23 +64,20 @@ export default {
     return {
       clipped: false,
       drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'camera'  ,
+          title: 'Cameras',
+          to: '/cameras'
         }
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Big Fiubrother'
     }
   }
 }
