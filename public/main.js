3c62b676c403475a2807c6f34f4563f4c1c92b7d
Eduardo Neira <edun327@gmail.com>
2020-01-03 01:45:49 -0300

    Added media source extension

--- public/main.js
+++ public/main.js
@@ -1,21 +1,44 @@
-// var mime = 'video/mp4; codecs="avc1.64001F"';
-// var mediaSource = new MediaSource();
+const PriorityQueue = require('./priority_queue')
 
-// var player = document.getElementById('video-player');
-// player.src = URL.createObjectURL(mediaSource);
+var mime = 'video/mp4; codecs="avc1.64001F"';
+var mediaSource = new MediaSource();
 
-// mediaSource.addEventListener('sourceopen', sourceOpen);
-
-// function sourceOpen(e) {
-//   URL.revokeObjectURL(player.src);
-
-//   var mediaSource = e.target;
-//   var sourceBuffer = mediaSource.addSourceBuffer(mime);
-
-// };
+var player = document.getElementById('video-player');
+player.src = URL.createObjectURL(mediaSource);
 
 var socket = io.connect('http://localhost:8080/video', { 'forceNew': true });
 
-socket.on('feed', function(data) {
-  console.log(data);
+mediaSource.addEventListener('sourceopen', function(e) {
+  URL.revokeObjectURL(player.src);
+  
+  var mediaSource = e.target;
+  mediaSource.duration = Number.POSITIVE_INFINITY
+  
+  var sourceBuffer = mediaSource.addSourceBuffer(mime);
+
+  var queue = new PriorityQueue(function (a,b) {
+    return a.timestamp > b.timestamp;
+  });
+
+  sourceBuffer.addEventListener('updateend', function(e) {
+    if (!sourceBuffer.updating && !queue.isEmpty()) {
+      var buffer = queue.pop().payload;
+      sourceBuffer.appendBuffer(buffer);
+    }
+  });
+
+  sourceBuffer.addEventListener('error', function(e) {
+    console.log(e);
+  });
+
+  socket.on('feed', function(data) {
+    if (sourceBuffer.updating) {
+      queue.push(data);
+    } else {
+      sourceBuffer.appendBuffer(data.payload);
+    }
+  });
 });
+
+
+
